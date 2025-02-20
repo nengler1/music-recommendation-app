dc749cc05e43c10273964cd2c82c6d7160d9e305
nengler <nengler@uccs.edu>
2025-02-20 12:51:43 -0700

    Fixed GET and POST requests and params

--- jsapp/app.js
+++ jsapp/app.js
@@ -1,18 +1,35 @@
 #!/usr/bin/env node
 
 const http = require('http')
+const url = require('url')
 
 const dancers = []
 
 const handleRequest = (req, res) => {
-	const [path, query] = req.url.split('?')
-	console.log(req.method)
-	if(req.method === "GET"){
+	const parsed = url.parse(req.url, true)
+	const path = parsed.pathname
+	const query = parsed.query
+
+	console.log("PATH", path)
+	console.log("QUERY", query)
+
+	if (req.method === "GET") {
 		console.log("- WOAH IN GET")
-		return sendResponse(res, dancers, 200)
+		if (Object.keys(query).length > 0) {	// if we get any parameters under GET response
+			const current_dancers = dancers.filter(item => 
+				item.who === query.who || 
+				item.x === query.x || 
+				item.y === query.y)
+			if (current_dancers.length > 0) {
+				return sendResponse(res, current_dancers, 200)
+			} else {
+				return sendResponse(res, { error: "Dancer not found." }, 404)
+			}
+		}
+		return sendResponse(res, dancers, 200)	// without any params, just return all dancers
 	}
 
-	if(req.method === "POST"){
+	if (req.method === "POST") {
 		console.log("- WOAH IN POST")
 		let body = ''
 		req.on('data', (data) => {
@@ -22,12 +39,16 @@ const handleRequest = (req, res) => {
 			const params = Object.fromEntries(body.split('&').map(
 				(param) => param.split('=')
 			))
+
+			if(!params.who || !params.x || !params.y){
+				return sendResponse(res, {error: "Missing parameters."}, 400)
+			}
 			dancers.push(params)
-			sendResponse(res, dancers, 200)
+			return sendResponse(res, dancers, 201)
 		})
 	}
 
-	if(req.method == "PUT"){
+	if (req.method == "PUT") {
 		console.log("- IN PUT!!")
 		let body = ''
 		req.on('data', (data) => {
@@ -39,9 +60,32 @@ const handleRequest = (req, res) => {
 			))
 			console.log(params)
 			const index = dancers.findIndex(name => name.who === params.who)
-			if(index != -1){
+			if (index != -1) {
 				console.log("WE HAVE A DANCER")
 				dancers[index] = params
+				sendResponse(res, dancers, 201)
+			} else {
+				console.log("NO DANCER")
+				sendResponse(res, { error: "Dancer not found." }, 404)
+			}
+		})
+	}
+
+	if (req.method == "DELETE") {
+		console.log("- IN DELETE!!")
+		let body = ''
+		req.on('data', (data) => {
+			body += data
+		})
+		req.on('end', () => {
+			const params = Object.fromEntries(body.split('&').map(
+				(param) => param.split('=')
+			))
+			console.log(params)
+			const index = dancers.findIndex(name => name.who === params.who)
+			if (index != -1) {
+				console.log("WE HAVE A DANCER")
+				dancers.splice(index, 1)
 				sendResponse(res, dancers, 200)
 			} else {
 				console.log("NO DANCER")
