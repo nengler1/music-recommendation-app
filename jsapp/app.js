fdacefaed94528529b8e0c2ff8af2c890a5f46b9
nengler <nengler@uccs.edu>
2025-02-19 22:46:49 -0700

    Added PUT

--- jsapp/app.js
+++ jsapp/app.js
@@ -1,34 +1,34 @@
 #!/usr/bin/env node
-const http = require("http")
+
+const http = require('http')
 
 const dancers = []
 
-const kbye = (res) => {
-	res.writeHead(200, {
-		"Content-Type": "application/json"
-	})
-	res.write(JSON.stringify(dancers))
-	res.end()
-}
-
 const handleRequest = (req, res) => {
 	const [path, query] = req.url.split('?')
-	if(req.method == "POST") {
+	if (req.method == "GET") {
+		return sendResponse(res, dancers, 200)
+	}
+	if (req.method == "POST") {
 		let body = ''
 		req.on('data', (data) => {
 			body += data
-			console.log(body)
 		})
 		req.on('end', () => {
 			const params = Object.fromEntries(body.split('&').map(
-			(param) => param.split('=')
+				(param) => param.split('=')
 			))
 			dancers.push(params)
-			kbye(res)
+			sendResponse(res, dancers, 200)
 		})
-	} else {
-		kbye(res)
 	}
-};
+}
+const sendResponse = (res, data, status_code) => {
+	res.writeHead(status_code, {
+		"Content-Type": "application/json"
+	})
+	res.write(JSON.stringify(data))
+	res.end()
+}
 const server = http.createServer(handleRequest)
-server.listen(3000);
+server.listen(3000)
