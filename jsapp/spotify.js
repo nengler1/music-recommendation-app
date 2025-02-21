960b3c225b50ec2e139429a1aabe4070fc80e6bf
nengler <nengler@uccs.edu>
2025-02-21 11:19:54 -0700

    Spotify API progress

---
+++ jsapp/spotify.js
@@ -0,0 +1,91 @@
+require('dotenv').config()
+const express = require('express')
+const SpotifyWebAPI = require('spotify-web-api-node')
+
+const app = express()
+const port = 3000
+
+const spotifyAPI = new SpotifyWebAPI({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI
+})
+
+app.get('/login', (req, res) => {
+    const scopes = [
+        'user-read-private',
+        'user-read-email',
+        'user-top-read',
+        'user-read-recently-played',
+        'user-read-playback-position'
+    ]
+    res.redirect(spotifyAPI.createAuthorizeURL(scopes))
+})
+
+app.get('/spotify.html', (req, res) => {
+    console.log("REDIRECTED")
+    const error = req.query.error
+    const code = req.query.code
+    const state = req.query.state
+
+    if(error){
+        console.error('Error:', error)
+        res.send(`Error: ${error}`)
+        return
+    }
+    spotifyAPI.authorizationCodeGrant(code).then(data => {
+        const accessToken = data.body.access_token
+        const refreshToken = data.body.refresh_token
+        const expiresIn = data.body.expires_in
+
+        spotifyAPI.setAccessToken(accessToken)
+        spotifyAPI.setRefreshToken(refreshToken)
+
+        console.log('Access Token:', accessToken)
+        console.log('Refresh Token:', refreshToken)
+
+        res.send("Success!")
+
+        setInterval(async() => {
+            const data = await spotifyAPI.refreshAccessToken()
+            const accessTokenRefreshed = data.body['access_token']
+            spotifyAPI.setAccessToken(accessTokenRefreshed)
+        }, (expiresIn / 2) * 1000)
+    }).catch(error => {
+        console.error('Error:', error)
+        res.send("Error getting token")
+    })
+})
+
+app.get('/me', (req, res) => {
+    spotifyAPI.getMe().then(data => {
+        console.log('Information:', data.body)
+        res.send(data.body)
+    }).catch(error => {
+        console.error('Error:', error)
+        res.send("Error has occured:", error)
+    })
+})
+
+app.get('/me/top-tracks', (req, res) => {
+    const {time_range} = req.query
+    console.log("QUERY:", time_range)
+    spotifyAPI.getMyTopTracks({time_range: time_range}).then(data => {
+        const tracks = data.body.items.map(track => ({
+            artist: track.artists.map(artist => artist.name).join(', '),
+            name: track.name,
+            album: track.album.name,
+            albumImage: track.album.images[0]?.url,
+            popularity: track.popularity
+        }))
+        console.log("Sent Tracks!")
+        res.send(tracks)
+    }).catch(error => {
+        console.error('Error:', error)
+        res.send("Error has occured:", error)
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+})
